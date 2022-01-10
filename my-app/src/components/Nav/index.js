caa1172a0f6aa86555437c84abd07be0a09e7415
AustinxEwell <austin.ewell86@gmail.com>
2022-01-10 15:04:08 -0700

    created basic CSS for navbar

--- my-app/src/components/Nav/index.js
+++ my-app/src/components/Nav/index.js
@@ -1,15 +1,18 @@
 import React from 'react'
+import Home from '../Home';
 
 export default function Nav(props) {
     const tabs = ['Home', 'Media Gallery', 'Services'];
 
     return (
-        <nav className="">
-        <div>
-          <h1>CRYSTADIANEPHOTOGRAPHY</h1>
-          <p>801-800-0771</p>
+        <nav className="flex-row">
+        <div className='container'>
+            <div className='vertical-center'>
+                <h1>CRYSTADIANEPHOTOGRAPHY</h1>
+                <p className='headerPhone'>801-800-0771</p>
+            </div>
         </div>
-        <ul className="">
+        <ul className="flex-row noBullets">
           {tabs.map(tab => (
             <li className="" key={tab}>
               <a
@@ -23,8 +26,16 @@ export default function Nav(props) {
               </a>
             </li>
           ))}
-          <button>More</button>
-          <button>Get In Touch</button>
+            <div class="dropdown">
+                <span className='more-button'>More</span>
+                <div class="dropdown-content">
+                    <ul className='dropdown-list'>
+                        <li><a className='navDropdownLink' href='#contact'>Contact</a></li>
+                        <li><a className='navDropdownLink' href='#quote'>Quote</a></li>
+                    </ul>
+                </div>
+            </div>
+          <a className='getInTouchButton' href='#contact'>Get In Touch</a>
         </ul>
       </nav>
     )
