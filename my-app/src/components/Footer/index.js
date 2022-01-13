cc70da00e8f5fa8aa643d6b26210af02d02de2e7
AustinxEwell <austin.ewell86@gmail.com>
2022-01-12 20:58:32 -0700

    modified nav and footer for better layout

--- my-app/src/components/Footer/index.js
+++ my-app/src/components/Footer/index.js
@@ -1,14 +1,34 @@
 import React from 'react'
 import './footer.css'
+import { NavLink } from 'react-router-dom'
+import { FaFacebookF, FaInstagram } from 'react-icons/fa'
 
 export default function Footer() {
     return (
-        <footer>
-            <div className="copyright">
-                &copy;by crystadianephotography.
+        <footer className='flex-row'>
+            <div className='contactFooter'>
+                <NavLink exact to="/Contact" className='nav-linkFooter' activeClassName='nav-linkActiveFooter'>Contact</NavLink>
                 <br />
-                Proudly created by <a className='copyrightLink' href='https://github.com/austinxewell' target='_blank'>Austin Ewell</a>
-            </div>    
+                <a className='emailFooter' href='mailto:crystadianephotography@gmail.com'>crystadianephotography@gmail.com</a>
+                <div>
+                    <a href='https://www.facebook.com/crystadianephotography' target='_blank'><FaFacebookF size='20px' /></a>
+                    <a href='https://www.instagram.com/crystadianephotography/' target='_blank'><FaInstagram size='20px' /></a>
+                </div>
+            </div>
+            <div className='mainNameFooter'>
+                <h1>
+                    CRYSTA DIANE
+                    <br />
+                    PHOTOGRAPHY
+                </h1>
+            </div>
+            <div className="copyright">
+                <div className='copyrightText'>
+                &copy;crystadianephotography
+                <br />
+                Created by: <a className='copyrightLink' href='https://github.com/austinxewell' target='_blank'>Austin Ewell</a>
+                </div>
+            </div>
         </footer>
     )
 }
--- my-app/src/components/Nav/index.js
+++ my-app/src/components/Nav/index.js
@@ -2,31 +2,24 @@ import React from 'react'
 import { NavLink } from 'react-router-dom';
 import "./nav.css"
 
-import { FaFacebookF, FaInstagram } from 'react-icons/fa'
+
 
 
 export default function Nav() {
-
     return (
         <header className="">
-            <div className='flex-row contact-header'>
-                <h3><a href='mailto:crystadianephotography@gmail.com'>crystadianephotography@gmail.com</a><span className='phone-span'>801-800-0771</span></h3>
-                <div className='media-icons'>
-                    <a href='http://www.Facebook.com/' target='_blank'><FaFacebookF size='20px' /></a>
-                    <a href='http://www.instagram.com' target='_blank'><FaInstagram size='20px' /></a>
+            <div className='flex-row navBar'>
+                <NavLink exact to="/" className='nav-link' activeClassName='nav-linkActive'>Home</NavLink>
+                <NavLink exact to="/mediagallery" className='nav-link' activeClassName='nav-linkActive'>Media Gallery</NavLink>
+                <div className='mainName'>
+                    <h1>
+                        CRYSTA DIANE
+                        <br />
+                        PHOTOGRAPHY
+                    </h1>
                 </div>
-            </div>
-            <div className='headerWrapper'>
-                <div className='header-font'>
-                    <h1>CRYSTADIANEPHOTOGRAPHY</h1>
-                    <p className='headerPhone'>801-800-0771</p>
-                    <div className='navBar'>
-                        <NavLink exact to="/" className='nav-link' activeClassName='nav-linkActive'>Home</NavLink>
-                        <NavLink exact to="/mediagallery" className='nav-link' activeClassName='nav-linkActive'>Media Gallery</NavLink>
-                        <NavLink exact to="/services" className='nav-link' activeClassName='nav-linkActive'>Services</NavLink>
-                        <NavLink exact to="/Contact" className='getInTouchButton' activeClassName='getInTouch-linkActive'>Get In Touch</NavLink>
-                    </div>
-                </div>
+                <NavLink exact to="/services" className='nav-link' activeClassName='nav-linkActive'>Services</NavLink>
+                <NavLink exact to="/Contact" className='nav-link' activeClassName='nav-linkActive'>Contact</NavLink>
             </div>
         </header>
     )
