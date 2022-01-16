bf8b76b2fa77404eb254ea77ab201fcc040d8354
AustinxEwell <austin.ewell86@gmail.com>
2022-01-16 01:37:03 -0700

    created underline on active review

--- my-app/src/components/Home/index.js
+++ my-app/src/components/Home/index.js
@@ -4,20 +4,27 @@ import References from '../../pages/References'
 import references from '../../pages/References/references.json'
 
 export default function Home() {
-    const [activeReference, setActiveReference] = useState(false)
+    const [activeReference, setActiveReference] = useState(-1)
     const [reference, setCurrentReference] = useState({
         fullName: "I'm Crysta Diane",
         referenceText: 'Reviews not only mean the world to me as a creative, but also can have a large impact on small businesses. A positive, honest review can make or break a potential decision for a prospective client. As a thank you for contributing to my small business by leaving a review, you will receive an automatic 20% discount on a future session (Weddings excluded). You can leave a review on social media, Google, or send me an email!',
-        referenceImgLink: 'pic4',
+        referenceImgLink: 'pic6',
         referenceImgAlt: 'Crysta Diane Photo'
     })
 
-    // const handleToggel = () => {
-    //     setCurrentReference(reference)
-    //     setActiveReference(!activeReference);
-    // }
+    const resetCrystaReference = () => {
+        setActiveReference(-1);
+        setCurrentReference({
+            fullName: "I'm Crysta Diane",
+            referenceText: 'Reviews not only mean the world to me as a creative, but also can have a large impact on small businesses. A positive, honest review can make or break a potential decision for a prospective client. As a thank you for contributing to my small business by leaving a review, you will receive an automatic 20% discount on a future session (Weddings excluded). You can leave a review on social media, Google, or send me an email!',
+            referenceImgLink: 'pic6',
+            referenceImgAlt: 'Crysta Diane Photo'
+        })
+    }
 
-    console.log(activeReference)
+    const handleOnClick = index => {
+        setActiveReference(index)
+    }
 
     return (
         <div className='homeWrapper'>
@@ -26,21 +33,11 @@ export default function Home() {
                 <h3 className='reviewsTitle'>•REVIEWS•</h3>
                 <div className='referenceCardWrapper'>
                     <ul className='references'>
-                        <p
-                            className='referenceName'
-                            onClick={() => setCurrentReference({
-                                fullName: "I'm Crysta Diane",
-                                referenceText: 'Reviews not only mean the world to me as a creative, but also can have a large impact on small businesses. A positive, honest review can make or break a potential decision for a prospective client. As a thank you for contributing to my small business by leaving a review, you will receive an automatic 20% discount on a future session (Weddings excluded). You can leave a review on social media, Google, or send me an email!',
-                                referenceImgLink: 'pic6',
-                                referenceImgAlt: 'Crysta Diane Photo'
-                            })}
-                        >CrystaDiane</p>
-                        {references.map((reference) => (
-                            <li
-                                onClick={() => setActiveReference(true)}
-                                className={
-                                    // activeReference ? 'referenceNameActive' :
-                                     'referenceName'}>
+                        <p className='referenceName' onClick={() => resetCrystaReference()}>CrystaDiane</p>
+                        {references.map((reference, index) => (
+                            <li 
+                            onClick={() => handleOnClick(index)}
+                                className={activeReference === index ? 'referenceNameActive' :'referenceName'}>
                                 <a onClick={() => setCurrentReference(reference)}>
                                     {reference.referenceName}
                                 </a>
